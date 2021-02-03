e12273c620e021f0921b416f56189b3c42f3564d
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2021-02-03 14:22:29 -0300

    soporte para GET /model y GET /model/:id

--- api.js
+++ api.js
@@ -1,17 +1,36 @@
 import fs from "fs";
 import Router from "@koa/router";
 
-const endpoints = (db) => {
-  const router = new Router();
-  const models = JSON.parse(fs.readFileSync("./models.json").toString());
-  Object.keys(models).forEach(model => {
-    const endpointName = model;
-    const endpointOptions = models[model];
-    const { tableName, properties } = endpointOptions;
-    const propertiesToFields = Object.entries(properties);
+const router = new Router();
+const models = JSON.parse(fs.readFileSync("./models.json").toString());
+
+const buildMethodMiddleware = {
+  get: ({ db, endpointName, tableName, propertiesToFields, primaryKey }) => {
     router.get(`/${endpointName}`, async (ctx) => {
       ctx.body = await db(tableName).select(propertiesToFields.map(([property, field]) => db.ref(field).as(property))).limit(10);
     });
+    router.get(`/${endpointName}/:id`, async (ctx) => {
+      [ctx.body] = await db(tableName).select(propertiesToFields.map(([property, field]) => db.ref(field).as(property))).where({ [primaryKey]: ctx.params.id });
+    });
+  },
+  post: (db, endpointName, tableName, propertiesToFields) => {
+
+  },
+  put: (db, endpointName, tableName, propertiesToFields) => {
+
+  },
+  delete: (db, endpointName, tableName, propertiesToFields) => {
+
+  }
+}
+
+const endpoints = (db) => {
+  Object.keys(models).forEach(model => {
+    const endpointName = model;
+    const endpointOptions = models[model];
+    const { tableName, properties, primaryKey, methods = ["get"] } = endpointOptions;
+    const propertiesToFields = Object.entries(properties);
+    methods.forEach(method => buildMethodMiddleware[method]({ db, router, endpointName, tableName, propertiesToFields, primaryKey }));
   });
   return router.routes();
 };
