f64dcb5bcd92d1aaec5420f7c675b92784e9e5bb
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2021-02-03 13:49:09 -0300

    punto de partida listo!

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM alpine:3.13.1
-
-RUN \
-  apk update; \
-  apk add mysql git \
--- index.js
+++ index.js
@@ -4,22 +4,28 @@ import Router from "@koa/router";
 import debug from "debug";
 import Knex from "knex";
 
+const { DB_HOST, DB_PORT, DB_USER, DB_PWD, DB_NAME } = process.env;
+
 const log = debug("api");
 const app = new Koa();
 const router = new Router();
 const db = Knex({
   client: "mysql2",
   connection: {
-    host: "confapi_db_server",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "chinook"
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PWD,
+    database: DB_NAME
   }
 });
 
 router.get("/artists", async (ctx) => {
-  ctx.body = await db("artists").select().limit(10);
+  ctx.body = await db("Artist").select().limit(10);
+});
+
+router.get("/tracks", async (ctx) => {
+  ctx.body = await db("Track").select().limit(10);
 });
 
 app.use(cors());
